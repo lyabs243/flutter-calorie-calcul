e08b81ff5f9b27d4f9cebdb8735a8440e82689f8
lyabs243 <loicyabili@outlook.com>
2019-07-20 12:32:40 +0200

    add scrollview on weight input

--- lib/main.dart
+++ lib/main.dart
@@ -64,130 +64,135 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           backgroundColor: getColor(),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              new Text(
-                'Fill all field to get needed calories!',
-                textScaleFactor: 1.3,
-              ),
-              new Card(
-                elevation: 15.0,
-                child: new Container(
-                  width: MediaQuery.of(context).size.width / 1.2,
-                  height: MediaQuery.of(context).size.height / 1.8,
-                  padding: EdgeInsets.all(10.0),
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      new Row(
-                        children: <Widget>[
-                          new Text(
-                            'Woman',
-                            style: new TextStyle(
-                              color: Colors.pink,
+        body: new SingleChildScrollView(
+          padding: EdgeInsets.only(top: 25.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Text(
+                  'Fill all field to get needed calories!',
+                  textScaleFactor: 1.3,
+                ),
+                new Padding(padding: EdgeInsets.only(top: 30.0)),
+                new Card(
+                  elevation: 15.0,
+                  child: new Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: MediaQuery.of(context).size.height / 1.8,
+                    padding: EdgeInsets.all(10.0),
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        new Row(
+                          children: <Widget>[
+                            new Text(
+                              'Woman',
+                              style: new TextStyle(
+                                color: Colors.pink,
+                              ),
                             ),
-                          ),
-                          new Switch(
-                            value: genderMale,
-                            inactiveTrackColor: (genderMale)? Colors.blue[300] : Colors.pink[300],
-                            activeColor: (genderMale)? Colors.blue : Colors.pink,
-                            onChanged: (bool gender){
-                              setState(() {
-                                genderMale = gender;
-                              });
-                            },
-                          ),
-                          new Text(
-                            'Man',
-                            style: new TextStyle(
-                              color: getColor(),
+                            new Switch(
+                              value: genderMale,
+                              inactiveTrackColor: (genderMale)? Colors.blue[300] : Colors.pink[300],
+                              activeColor: (genderMale)? Colors.blue : Colors.pink,
+                              onChanged: (bool gender){
+                                setState(() {
+                                  genderMale = gender;
+                                });
+                              },
                             ),
+                            new Text(
+                              'Man',
+                              style: new TextStyle(
+                                color: getColor(),
+                              ),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        ),
+                        new RaisedButton(
+                          onPressed: (){
+                            showDate();
+                          },
+                          child: new Text(
+                              'Your age is ${age?? ''}'
                           ),
-                        ],
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      ),
-                      new RaisedButton(
-                        onPressed: (){
-                          showDate();
-                        },
-                        child: new Text(
-                            'Your age is ${age?? ''}'
-                        ),
-                        color: getColor(),
-                        textColor: Colors.white,
-                      ),
-                      new Text(
-                        'Your Size is ${size?? ''} cm',
-                        style: new TextStyle(
                           color: getColor(),
+                          textColor: Colors.white,
                         ),
-                      ),
-                      new Slider(
-                        value: size.toDouble(),
-                        activeColor: getColor(),
-                        min: 1.0,
-                        max: 250.0,
-                        onChanged: (double _size){
-                          setState(() {
-                            size = _size.floor();
-                          });
-                        },
-                      ),
-                      new TextField(
-                        keyboardType: TextInputType.number,
-                        onChanged: (String text){
-                          setState(() {
-                            weight = int.parse(text);
-                          });
-                        },
-                        onSubmitted: (String text){
-                          setState(() {
-                            weight = int.parse(text);
-                          });
-                        },
-                        decoration: new InputDecoration(
-                          labelText: 'Your weight in Kg',
+                        new Text(
+                          'Your Size is ${size?? ''} cm',
+                          style: new TextStyle(
+                            color: getColor(),
+                          ),
                         ),
-                      ),
-                      new Text(
-                        'Which Sport do you do?',
-                        style: new TextStyle(
-                          color: getColor(),
+                        new Slider(
+                          value: size.toDouble(),
+                          activeColor: getColor(),
+                          min: 1.0,
+                          max: 250.0,
+                          onChanged: (double _size){
+                            setState(() {
+                              size = _size.floor();
+                            });
+                          },
                         ),
-                      ),
-                      new Row(
-                        children: getSportActivities(),
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      ),
-                    ],
+                        new TextField(
+                          keyboardType: TextInputType.number,
+                          onChanged: (String text){
+                            setState(() {
+                              weight = int.parse(text);
+                            });
+                          },
+                          onSubmitted: (String text){
+                            setState(() {
+                              weight = int.parse(text);
+                            });
+                          },
+                          decoration: new InputDecoration(
+                            labelText: 'Your weight in Kg',
+                          ),
+                        ),
+                        new Text(
+                          'Which Sport do you do?',
+                          style: new TextStyle(
+                            color: getColor(),
+                          ),
+                        ),
+                        new Row(
+                          children: getSportActivities(),
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              new RaisedButton(
-                onPressed: (){
-                  if(age == null || weight == null || sportActivitySelected == null){
-                    showAlert(context, 'Warning', 'You must fill all fields!');
-                  }
-                  else{
-                    double cal =  (genderMale)?
-                  66.4730 + (13.7516 * weight) + (5.0033 * size) - (6.7550 * age) :
-                    655.0955 + (9.5634 * weight) + (1.8496 * size) - (4.6756 * age);
+                new Padding(padding: EdgeInsets.only(top: 30.0)),
+                new RaisedButton(
+                  onPressed: (){
+                    if(age == null || weight == null || sportActivitySelected == null){
+                      showAlert(context, 'Warning', 'You must fill all fields!');
+                    }
+                    else{
+                      double cal =  (genderMale)?
+                      66.4730 + (13.7516 * weight) + (5.0033 * size) - (6.7550 * age) :
+                      655.0955 + (9.5634 * weight) + (1.8496 * size) - (4.6756 * age);
 
-                    double sportCal = sportActivities[sportActivitySelected]['rate']*cal;
+                      double sportCal = sportActivities[sportActivitySelected]['rate']*cal;
 
-                    showAlert(context, 'Result', 'You need ${cal.round()} calories, relate to your sport activities, you needs ${sportCal.round()} calories.');
-                  }
-                },
-                child: new Text(
-                    'Calculate'
+                      showAlert(context, 'Result', 'You need ${cal.round()} calories, relate to your sport activities, you needs ${sportCal.round()} calories.');
+                    }
+                  },
+                  child: new Text(
+                      'Calculate'
+                  ),
+                  elevation: 15.0,
+                  color: getColor(),
+                  textColor: Colors.white,
                 ),
-                elevation: 15.0,
-                color: getColor(),
-                textColor: Colors.white,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
